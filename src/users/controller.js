1824343ece27b18f874f71685b1f6d8d838a6e44
hakanerdogan085 <hakanerdogan085@gmail.com>
2023-02-17 10:58:50 +0300

    edit

--- src/users/controller.js
+++ src/users/controller.js
@@ -7,15 +7,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
-
 const getUsers = (req, res) => {
     pool.select('*')
         .from('USERLOGIN')
