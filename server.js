6b5809af729598ad34501bce297781ddf143c250
hakanerdogan085 <hakanerdogan085@gmail.com>
2023-02-17 10:54:24 +0300

    edit

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+
 
 app.use("/api", usersRoutes);
 
