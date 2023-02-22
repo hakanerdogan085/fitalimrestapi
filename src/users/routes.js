abff7ed3b1a7f446a960b46ad55d661e4a2325e7
hakanerdogan085 <hakanerdogan085@gmail.com>
2023-02-22 17:22:25 +0300

    Edit

--- src/users/routes.js
+++ src/users/routes.js
@@ -6,5 +6,4 @@ const router = Router();
 router.get("/users", controller.getUsers);
 router.get("/cequipment", controller.getData);
 router.get("/cequipment/:equipmentcategory", controller.getDataFilter);
-
 module.exports = router;
